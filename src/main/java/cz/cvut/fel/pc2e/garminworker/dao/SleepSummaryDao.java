46e05a49109295866b5d09284fd4de3376a5522c
Skalická, Anna <skalian1@fel.cvut.cz>
2022-05-01 21:46:15 +0200

    updated sleep summary

--- src/main/java/cz/cvut/fel/pc2e/garminworker/dao/SleepSummaryDao.java
+++ src/main/java/cz/cvut/fel/pc2e/garminworker/dao/SleepSummaryDao.java
@@ -4,6 +4,7 @@ import cz.cvut.fel.pc2e.garminworker.entities.sleeps.SleepSummary;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.NoResultException;
+import java.util.List;
 
 @Repository
 public class SleepSummaryDao extends BaseDao<SleepSummary> {
@@ -17,6 +18,18 @@ public class SleepSummaryDao extends BaseDao<SleepSummary> {
             s = em.createNamedQuery("SleepSummary.findBySummaryId", SleepSummary.class).setParameter("summaryId", summaryId)
                     .getSingleResult();
             return s;
+        } catch (NoResultException e) {
+            return null;
+        }
+
+    }
+
+    public SleepSummary findBySummaryUserAndDate(String userId, Long startTime) {
+        try {
+            return em.createNamedQuery("SleepSummary.findBySummaryUserAndDate", SleepSummary.class)
+                    .setParameter("userId", userId)
+                    .setParameter("startTime", startTime)
+                    .getSingleResult();
         } catch (NoResultException e) {
             return null;
         }
