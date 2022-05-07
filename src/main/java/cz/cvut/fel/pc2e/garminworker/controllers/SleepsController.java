28dbaa9bf1367c10417d90a3aa4fdae78ae687fb
Patrik Pavelka <pavelpa2@fel.cvut.cz>
2022-05-07 14:10:08 +0200

    secured get sleep summary by id endpoint

--- src/main/java/cz/cvut/fel/pc2e/garminworker/controllers/SleepsController.java
+++ src/main/java/cz/cvut/fel/pc2e/garminworker/controllers/SleepsController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
@@ -26,6 +27,7 @@ public class SleepsController {
     }
 
     @GetMapping(value = "/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public SleepSummary getSleepSummaryById(@PathVariable Integer id) {
         return sleepsService.getSleepSummaryById(id);
     }
