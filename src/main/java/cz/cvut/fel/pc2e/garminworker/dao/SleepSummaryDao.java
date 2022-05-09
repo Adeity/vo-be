50e13ccf34f5c98716df477b02403df282b5fb11
Patrik Pavelka <pavelpa2@fel.cvut.cz>
2022-05-09 08:31:44 +0200

    switched to JpaRepository library on SleepSummaryDao

--- src/main/java/cz/cvut/fel/pc2e/garminworker/dao/SleepSummaryDao.java
+++ src/main/java/cz/cvut/fel/pc2e/garminworker/dao/SleepSummaryDao.java
@@ -1,41 +1,12 @@
 package cz.cvut.fel.pc2e.garminworker.dao;
 
+import cz.cvut.fel.pc2e.garminworker.entities.DeviceEntity;
 import cz.cvut.fel.pc2e.garminworker.entities.sleeps.SleepSummary;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.NoResultException;
-import java.util.List;
+import java.util.Optional;
 
-@Slf4j
-@Repository
-public class SleepSummaryDao extends BaseDao<SleepSummary> {
-    protected SleepSummaryDao() {
-        super(SleepSummary.class);
-    }
-
-    public SleepSummary findBySummaryId(String summaryId) {
-        SleepSummary s;
-        try {
-            s = em.createNamedQuery("SleepSummary.findBySummaryId", SleepSummary.class).setParameter("summaryId", summaryId)
-                    .getSingleResult();
-            return s;
-        } catch (NoResultException e) {
-            return null;
-        }
-
-    }
-
-    public SleepSummary findBySummaryUserAndDate(String userId, Long startTime) {
-        try {
-            return em.createNamedQuery("SleepSummary.findBySummaryUserAndDate", SleepSummary.class)
-                    .setParameter("userId", userId)
-                    .setParameter("startTime", startTime)
-                    .getSingleResult();
-        } catch (NoResultException e) {
-            log.info("No existing data with userId ${} and start time ${} found", userId, startTime);
-            return null;
-        }
-
-    }
+public interface SleepSummaryDao extends JpaRepository<SleepSummary, Integer> {
+    Optional<SleepSummary> findBySummaryId(String deviceId);
 }
